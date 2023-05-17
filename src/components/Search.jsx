c943881e3e11605875acecdf8d87c873406a6b64
riddhi <riddhivekariya3@gmail.com>
2023-05-17 18:38:24 +0200

    adding Searchbar

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -3,9 +3,14 @@ import { FaSearch } from "react-icons/fa";
 
 const Search = () => {
   return (
-    <div className="input-wrapper">
-      <FaSearch className="flex" />
-      <input type="text" placeholder="Type a search...." />
+    <div className="flex flex-row h-10 my-1">
+      <input
+        className="rounded-full pl-5 w-60 relative"
+        type="text"
+        placeholder="Search...."
+      />
+      <hr className="bg-slate-400 h-1 absolute h-[2.5rem] w-px ml-48" />
+      <FaSearch className="absolute h-10 ms-20 ml-52 hover:bg-slate-300 w-5 cursor-pointer" />
     </div>
   );
 };
